bd34bda14b9429fe7d642944aa02685c02bed29d
Thomas Ferris <thomaskeithferris@gmail.com>
2019-10-14 19:44:34 -0700

    working with EJS

--- index.js
+++ index.js
@@ -8,43 +8,32 @@ var request = require("request");
 
 const config = require("./config.js").token;
 
-// app.use(express.static("views"));
-// app.use(express.static("styles"));
-
-// app.get("/", (req, res) => res.render("index.ejs"));
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-// http
-//   .get(
-//     `http://api.openweathermap.org/data/2.5/forecast?id=6173331&APPID=${config}`,
-//     res => {
-//       console.log(res);
-//     }
-//   )
-//   .on("socket", socket => {
-//     socket.emit("agentRemove");
-//   });
-
 const apiCall = `http://api.openweathermap.org/data/2.5/weather?id=6173331&APPID=${config}`;
 
 let results = null;
-let rainValue = false;
-
-request(apiCall, function(error, response, body) {
-  console.log("error:", error); // Print the error if one occurred
-  responce = JSON.parse(body);
-  results = responce.weather[0];
-  findMyRain(results);
-});
-
-const findMyRain = apiResult => {
-  console.log("this is a calllback", apiResult);
-  let rainRegEx = RegExp("w*rain|Rainw*");
-  console.log(rainRegEx.test(apiResult.main));
-  console.log(rainRegEx.test(apiResult.description));
+let rainValue = null;
+const init = () => {
+  request(apiCall, function(error, response, body) {
+    console.log("error:", error); // Print the error if one occurred
+    responce = JSON.parse(body);
+    results = responce.weather[0];
+    findMyRain(results);
+  });
 };
 
-// { id: 6173331,
-//     name: 'Vancouver',
-//     country: 'CA',
-//     coord: { lon: -123.119339, lat: 49.24966 } }
+const findMyRain = apiResult => {
+  console.log(apiResult);
+  let rainRegEx = RegExp("w*rain|Rainw*");
+  let a = rainRegEx.test(apiResult.main);
+  let b = rainRegEx.test(apiResult.description);
+  let c = rainRegEx.test("periodic rain");
+  a || b === true ? (rainValue = true) : (rainValue = false);
+  return rainValue;
+};
+
+app.use(express.static("views"));
+app.use(express.static("styles"));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.get("/", (req, res) => res.render("index.ejs", { rainValue: init() }));
