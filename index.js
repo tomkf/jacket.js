49d22a97a9dd3296c600c7789ec08202124cb197
Thomas Ferris <thomaskeithferris@gmail.com>
2019-10-14 19:51:51 -0700

    working

--- index.js
+++ index.js
@@ -12,21 +12,18 @@ const apiCall = `http://api.openweathermap.org/data/2.5/weather?id=6173331&APPID
 
 let results = null;
 let rainValue = null;
-const init = () => {
-  request(apiCall, function(error, response, body) {
-    console.log("error:", error); // Print the error if one occurred
-    responce = JSON.parse(body);
-    results = responce.weather[0];
-    findMyRain(results);
-  });
-};
+request(apiCall, function(error, response, body) {
+  console.log("error:", error); // Print the error if one occurred
+  responce = JSON.parse(body);
+  results = responce.weather[0];
+  findMyRain(results);
+});
 
 const findMyRain = apiResult => {
   console.log(apiResult);
   let rainRegEx = RegExp("w*rain|Rainw*");
   let a = rainRegEx.test(apiResult.main);
   let b = rainRegEx.test(apiResult.description);
-  let c = rainRegEx.test("periodic rain");
   a || b === true ? (rainValue = true) : (rainValue = false);
   return rainValue;
 };
@@ -36,4 +33,9 @@ app.use(express.static("styles"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.get("/", (req, res) => res.render("index.ejs", { rainValue: init() }));
+app.get("/", (req, res) => {
+  let template = {
+    rainValue
+  };
+  res.render("index.ejs", template);
+});
